ee710540f55a13b4ab8474dcfcf5a8523f3efbad
hareeda <hareeda33@gmail.com>
2025-03-22 01:01:44 +0530

    Updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,13 +6,12 @@ function Navbar() {
   return (
     <nav className="navbar">
       <ul className="nav-links">
-        <li><Link to="#/">Home</Link></li>
-        <li><Link to="#/about">About</Link></li>
-        <li><Link to="#/projects">Projects</Link></li>
-        <li><Link to="#/experience">Experience</Link></li>
-        <li><Link to="#/education">Education</Link></li>
-        <li><Link to="#/contact">Contact</Link></li>
-
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/projects">Projects</Link></li>
+        <li><Link to="/experience">Experience</Link></li>
+        <li><Link to="/education">Education</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
       </ul>
     </nav>
   );
